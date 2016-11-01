976b9fea4d9268495acd3f7c57ad0c54bff5d142
Andreas Lüdemann <andreaslydemann@gmail.com>
2016-11-01 19:20:28 +0100

    Added Category

---
+++ I4PRJ SmartStorage/Models/Domain/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace I4PRJ_SmartStorage.Models.Domain
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+    }
+}
