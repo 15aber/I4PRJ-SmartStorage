43c7bae7d5512e4b3a9fa2277e2a7ccc0b3c5990
Jeppe Stærk <jeppe@staerk.io>
2016-11-01 10:59:23 +0100

    Init Sandbox Branch

--- I4PRJ SmartStorage/Models/ApplicationDbContext.cs
+++ I4PRJ SmartStorage/Models/ApplicationDbContext.cs
@@ -1,20 +1,10 @@
-﻿using I4PRJ_SmartStorage.Models.Domain;
-using Microsoft.AspNet.Identity.EntityFramework;
-using System.Data.Entity;
+﻿using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace I4PRJ_SmartStorage.Models
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
-        public DbSet<Product> Products { get; set; }
 
-        public DbSet<Category> Categories { get; set; }
-
-        public DbSet<Inventory> Inventories { get; set; }
-
-        public DbSet<Stock> Stocks { get; set; }
-
-        public DbSet<Transaction> Transactions { get; set; }
 
 
         public ApplicationDbContext()
