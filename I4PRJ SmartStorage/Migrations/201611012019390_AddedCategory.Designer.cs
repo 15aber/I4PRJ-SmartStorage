b32e068204c7edd86bb6aa7669ce5686e3992b5f
Andreas Lüdemann <andreaslydemann@gmail.com>
2016-11-01 21:22:11 +0100

    Updated category

--- I4PRJ SmartStorage/Migrations/201611012019390_AddedCategory.Designer.cs
+++ I4PRJ SmartStorage/Migrations/201611012019390_AddedCategory.Designer.cs
@@ -7,13 +7,13 @@ namespace I4PRJ_SmartStorage.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class InitialModel : IMigrationMetadata
+    public sealed partial class AddedCategory : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(InitialModel));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(AddedCategory));
         
         string IMigrationMetadata.Id
         {
-            get { return "201611011228450_InitialModel"; }
+            get { return "201611012019390_AddedCategory"; }
         }
         
         string IMigrationMetadata.Source
