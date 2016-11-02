ed5f8509e37ea69bb0c7f3725a671b3379b9509c
Jeppe Stærk <jeppe@staerk.io>
2016-11-02 08:47:55 +0100

    ProductViewModel Added

--- I4PRJ SmartStorage/Migrations/201611020728143_CategoryIsActivAdded.Designer.cs
+++ I4PRJ SmartStorage/Migrations/201611020728143_CategoryIsActivAdded.Designer.cs
@@ -7,13 +7,13 @@ namespace I4PRJ_SmartStorage.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class PropUpdate : IMigrationMetadata
+    public sealed partial class CategoryIsActivAdded : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(PropUpdate));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(CategoryIsActivAdded));
         
         string IMigrationMetadata.Id
         {
-            get { return "201611011357409_PropUpdate"; }
+            get { return "201611020728143_CategoryIsActivAdded"; }
         }
         
         string IMigrationMetadata.Source
--- I4PRJ SmartStorage/Models/Domain/Category.cs
+++ I4PRJ SmartStorage/Models/Domain/Category.cs
@@ -4,28 +4,31 @@ using System.ComponentModel.DataAnnotations;
 
 namespace I4PRJ_SmartStorage.Models.Domain
 {
-    public class Category
-    {
-        [Key]
-        [DisplayName("#")]
-        public int CategoryId { get; set; }
+  public class Category
+  {
+    [Key]
+    [DisplayName("#")]
+    public int CategoryId { get; set; }
 
-        [Required]
-        [DisplayName("Category")]
-        public string Name { get; set; }
+    [Required]
+    [DisplayName("Category")]
+    public string Name { get; set; }
 
-        [DisplayName("Updated")]
-        [DataType(DataType.DateTime)]
-        [DisplayFormat(DataFormatString = "{0:G}")]
-        [Editable(false)]
-        public DateTime Updated { get; set; }
+    [DisplayName("Updated")]
+    [DataType(DataType.DateTime)]
+    [DisplayFormat(DataFormatString = "{0:G}")]
+    [Editable(false)]
+    public DateTime Updated { get; set; }
 
-        [DisplayName("By")]
-        [Editable(false)]
-        public string ByUser { get; set; }
+    [DisplayName("By")]
+    [Editable(false)]
+    public string ByUser { get; set; }
 
-        //[Timestamp]
-        //[HiddenInput(DisplayValue = false)]
-        //public byte[] Version { get; set; }
-    }
+    [DisplayName("Activ")]
+    public bool IsActiv { get; set; }
+
+    //[Timestamp]
+    //[HiddenInput(DisplayValue = false)]
+    //public byte[] Version { get; set; }
+  }
 }
