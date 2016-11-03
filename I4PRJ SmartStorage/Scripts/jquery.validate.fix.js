d15fefb500b6e2868d385816f2c8805234697460
Jeppe Stærk <jeppe@staerk.io>
2016-11-03 20:24:20 +0100

    Jquery Validate error class updatet to bootstrap error class

--- I4PRJ SmartStorage/Scripts/jquery.validate.fix.js
+++ I4PRJ SmartStorage/Scripts/jquery.validate.fix.js
@@ -6,3 +6,14 @@
 $.validator.methods.number = function (value, element) {
   return this.optional(element) || /^-?(?:\d+|\d{1,3}(?:[\s\.,]\d{3})+)(?:[\.,]\d+)?$/.test(value);
 }
+
+$.validator.setDefaults({
+  errorClass: "has-error",
+  validClass: "has-success",
+  highlight: function (element) {
+    jQuery(element).closest('.form-group').addClass('has-error').removeClass('has-success');
+  },
+  unhighlight: function (element) {
+    jQuery(element).closest('.form-group').removeClass('has-error').addClass('has-success');
+  }
+});
