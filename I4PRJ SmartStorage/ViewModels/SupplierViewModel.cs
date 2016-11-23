7857e309ad13ae50cb56b34e8f53aa3e7eb0b543
Richard Joakim Key <201500155@post.au.dk>
2016-11-23 22:41:54 +0100

    ViewModel updates

---
+++ I4PRJ SmartStorage/ViewModels/SupplierViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using I4PRJ_SmartStorage.Models.Domain;
+
+namespace I4PRJ_SmartStorage.ViewModels
+{
+    public class SupplierViewModel
+    {
+        public List<Supplier> Suppliers { get; set; }
+        //public Product Products { get; set; }
+        public DateTime DeliveryDate { get; set; }
+    }
+}
