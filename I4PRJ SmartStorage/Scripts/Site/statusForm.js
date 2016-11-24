e90e50ff307dad978b9fbe76298ab556d3703b1c
VictorBusk <victorbusk@hotmail.com>
2016-11-24 15:41:27 +0100

    Difference implemented

--- I4PRJ SmartStorage/Scripts/Site/statusForm.js
+++ I4PRJ SmartStorage/Scripts/Site/statusForm.js
@@ -1,9 +1,10 @@
 ﻿$(document)
-    .ready(function() {
+    .ready(function () {
+
         var table = $("#status")
             .DataTable({
                 ajax: {
-                    url: "/api/status/1",
+                    url: "/api/status/GetStatus/1",
                     dataSrc: ""
                 },
                 "columnDefs": [
@@ -21,34 +22,48 @@
                         data: "product.category.name"
                     },
                     {
-                        data: "quantity"
-                    },
-                    {
-                        render: function() {
-                            return "<input type='number' style='width: 100%' class='form-control quantity'/>";
+                        data: "quantity",
+                        render: function(data) {
+                            return "<label id='expected'>" + data + "</label>";
                         }
                     },
                     {
-                        data: "quantity"
+                        render: function() {
+                            return "<input type='number' min='0' style='width: 100%' id='tast' class='form-control quantity'/>";
+                        }
+                    },
+                    {
+
+
                     }
                 ]
             });
 
-        $('.quantity')
-            .on('focusout',
-                function() {
-                    alert("it worked");
-                });
-        $('#quantity')
-            .focusout(function() {
-                alert("it worked");
-            });
+        $('#status').on('change', '.quantity', function (eventArg) {
+            var cells = $(this).closest('tr').children('td');
+            var value1 = document.getElementById("expected").textContent;
+            var value2 = cells.eq(3).find('input').val();
+            var diff = Math.abs(new Number(value1) - new Number(value2));
+            cells.eq(4).text(diff);
+        });
 
-        $('#quantity')
-            .addEventListener("focusout",
-                function() {
-                    alert("it worked");
-                });
+        //cells.eq(4).text(new Number(value1) - new Number(value2));
+
+        //$('.quantity')
+        //    .on('focusout',
+        //        function() {
+        //            alert("it worked");
+        //        });
+        //$('#quantity')
+        //    .focusout(function() {
+        //        alert("it worked");
+        //    });
+
+        //$('#quantity')
+        //    .addEventListener("focusout",
+        //        function() {
+        //            alert("it worked");
+        //        });
 
         $('#newStatus').validate({
             submitHandler: function () {
