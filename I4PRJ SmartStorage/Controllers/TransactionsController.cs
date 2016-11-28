d5519e63233ffa3522aeddc7090f0c7bde9d9961
Andreas Lüdemann <andreaslydemann@gmail.com>
2016-11-28 22:19:58 +0100

    Added Transaction

--- I4PRJ SmartStorage/Controllers/TransactionsController.cs
+++ I4PRJ SmartStorage/Controllers/TransactionsController.cs
@@ -9,167 +9,183 @@ using I4PRJ_SmartStorage.ViewModels;
 
 namespace I4PRJ_SmartStorage.Controllers
 {
-  public class TransactionsController : Controller
-  {
-    private ApplicationDbContext db = new ApplicationDbContext();
-
-    public ActionResult Index()
+    public class TransactionsController : Controller
     {
-      var viewModel = new TransactionViewModel
-      {
-        Transactions =
-              db.Transactions.Include(t => t.Product)
-                  .Include(t => t.FromInventory)
-                  .Include(t => t.ToInventory)
-                  .ToList()
-      };
+        private ApplicationDbContext db = new ApplicationDbContext();
 
-      return View(viewModel);
-    }
-
-    public ActionResult New(TransactionViewModel viewModel)
-    {
-      if(viewModel.IsChecked)
-      {
-        viewModel.Transaction = new Transaction();
-        viewModel.ToInventory = db.Inventories.Where(p => p.IsDeleted == false).ToList();
-        viewModel.Products = db.Products.Where(p => p.IsDeleted == false).ToList();
-
-        return View("RestockForm", viewModel);
-      }
-      else
-      {
-        viewModel.Transaction = new Transaction();
-        viewModel.FromInventory = db.Inventories.Where(p => p.IsDeleted == false).ToList();
-        viewModel.ToInventory = db.Inventories.Where(p => p.IsDeleted == false).ToList();
-        viewModel.Products = db.Products.Where(p => p.IsDeleted == false).ToList();
-
-        return View("TransactionsForm", viewModel);
-      }
-    }
-
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public ActionResult SaveRestock(Transaction transaction)
-    {
-      if(!ModelState.IsValid)
-      {
-        var viewModel = new TransactionViewModel
+        public ActionResult Index()
         {
-          ToInventory = db.Inventories.Where(p => p.IsDeleted == false).ToList(),
-          Products = db.Products.Where(p => p.IsDeleted == false).ToList()
-        };
+            var viewModel = new TransactionViewModel
+            {
+                Transactions =
+                    db.Transactions.Include(t => t.Product)
+                        .Include(t => t.FromInventory)
+                        .Include(t => t.ToInventory)
+                        .ToList()
+            };
 
-        return View("RestockForm", viewModel);
-      }
+            return View(viewModel);
+        }
 
-      var toStockInDb = db.Stocks.Include(s => s.Inventory)
-              .Include(s => s.Product)
-              .Where(s => s.InventoryId == transaction.ToInventoryId)
-              .Single(s => s.ProductId == transaction.ProductId);
-
-      if(toStockInDb == null)
-      {
-        var toStock = new Stock
+        public ActionResult NewRestock()
         {
-          InventoryId = transaction.ToInventoryId,
-          ProductId = transaction.ProductId,
-          Quantity = transaction.Quantity
-        };
+            var viewModel = new TransactionViewModel
+            {
+                ToInventory = db.Inventories.Where(p => p.IsDeleted == false).ToList(),
+                Products = db.Products.Where(p => p.IsDeleted == false).ToList()
+            };
 
-        db.Stocks.Add(toStock);
-      }
+            return View("RestockForm", viewModel);
+        }
 
-      else
-        toStockInDb.Quantity += transaction.Quantity;
+        public ActionResult NewTransaction()
+        {
+            var viewModel = new TransactionViewModel
+            {
+                FromInventory = db.Inventories.Where(p => p.IsDeleted == false).ToList(),
+                ToInventory = db.Inventories.Where(p => p.IsDeleted == false).ToList(),
+                Products = db.Products.Where(p => p.IsDeleted == false).ToList()
+            };
 
-      transaction.Updated = DateTime.Now;
-      transaction.ByUser = User.Identity.Name;
-      db.Transactions.Add(transaction);
+            return View("TransactionForm", viewModel);
+        }
 
-      db.SaveChanges();
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SaveRestock(TransactionViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                viewModel.FromInventory = db.Inventories.Where(p => p.IsDeleted == false).ToList();
+                viewModel.ToInventory = db.Inventories.Where(p => p.IsDeleted == false).ToList();
+                viewModel.Products = db.Products.Where(p => p.IsDeleted == false).ToList();
 
-      return RedirectToAction("Index", "Transactions");
+                return View("RestockForm", viewModel);
+            }
+
+            var transaction = new Transaction
+            {
+                ToInventoryId = viewModel.ToInventoryId,
+                ProductId = viewModel.ProductId,
+                Quantity = viewModel.Quantity
+            };
+
+            var toStockInDb = db.Stocks.Include(s => s.Inventory)
+                    .Include(s => s.Product)
+                    .Where(s => s.InventoryId == transaction.ToInventoryId)
+                    .Single(s => s.ProductId == transaction.ProductId);
+
+            if (toStockInDb == null)
+            {
+                var toStock = new Stock
+                {
+                    InventoryId = transaction.ToInventoryId,
+                    ProductId = transaction.ProductId,
+                    Quantity = transaction.Quantity
+                };
+
+                db.Stocks.Add(toStock);
+            }
+
+            else
+                toStockInDb.Quantity += transaction.Quantity;
+
+            transaction.Updated = DateTime.Now;
+            transaction.ByUser = User.Identity.Name;
+            db.Transactions.Add(transaction);
+
+            db.SaveChanges();
+
+            return RedirectToAction("Index", "Transactions");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SaveTransaction(TransactionViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                viewModel.FromInventory = db.Inventories.Where(p => p.IsDeleted == false).ToList();
+                viewModel.ToInventory = db.Inventories.Where(p => p.IsDeleted == false).ToList();
+                viewModel.Products = db.Products.Where(p => p.IsDeleted == false).ToList();
+
+                return View("TransactionForm", viewModel);
+            }
+
+            var quantity = db.Stocks.Single(s => s.InventoryId == viewModel.FromInventoryId && s.ProductId == viewModel.ProductId).Quantity;
+
+            if (viewModel.Quantity > quantity)
+            {
+                ModelState.AddModelError("", "Quantity exceeds the existing quantity: " + quantity + " in stock.");
+
+                viewModel.FromInventory = db.Inventories.Where(p => p.IsDeleted == false).ToList();
+                viewModel.ToInventory = db.Inventories.Where(p => p.IsDeleted == false).ToList();
+                viewModel.Products = db.Products.Where(p => p.IsDeleted == false).ToList();
+
+                return View("TransactionForm", viewModel);
+            }
+
+            var transaction = new Transaction
+            {
+                FromInventoryId = viewModel.FromInventoryId,
+                ToInventoryId = viewModel.ToInventoryId,
+                ProductId = viewModel.ProductId,
+                Quantity = viewModel.Quantity
+            };
+
+            var fromStockInDb = db.Stocks.Include(i => i.Inventory)
+                    .Include(p => p.Product)
+                    .Where(i => i.InventoryId == transaction.FromInventoryId)
+                    .Single(p => p.ProductId == transaction.ProductId);
+
+            if (fromStockInDb == null)
+            {
+                viewModel.FromInventory = db.Inventories.Where(p => p.IsDeleted == false).ToList();
+                viewModel.ToInventory = db.Inventories.Where(p => p.IsDeleted == false).ToList();
+                viewModel.Products = db.Products.Where(p => p.IsDeleted == false).ToList();
+
+                return View("TransactionForm", viewModel);
+            }
+
+            else
+                fromStockInDb.Quantity -= transaction.Quantity;
+
+            var toStockInDb = db.Stocks.Include(s => s.Inventory)
+                .Include(s => s.Product)
+                .Where(s => s.InventoryId == transaction.ToInventoryId)
+                .SingleOrDefault(s => s.ProductId == transaction.ProductId);
+
+            if (toStockInDb == null)
+            {
+                var toStock = new Stock
+                {
+                    InventoryId = transaction.ToInventoryId,
+                    ProductId = transaction.ProductId,
+                    Quantity = transaction.Quantity
+                };
+
+                db.Stocks.Add(toStock);
+            }
+
+            else
+                toStockInDb.Quantity += transaction.Quantity;
+
+            transaction.Updated = DateTime.Now;
+            transaction.ByUser = User.Identity.Name;
+            db.Transactions.Add(transaction);
+
+            db.SaveChanges();
+
+            return RedirectToAction("Index", "Transactions");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
     }
-
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public ActionResult SaveTransaction(Transaction transaction)
-    {
-      if(!ModelState.IsValid)
-      {
-        var inventoriesInDb = db.Inventories.Where(p => p.IsDeleted == false).ToList();
-        var productsInDb = db.Products.Where(p => p.IsDeleted == false).ToList();
-
-        var viewModel = new TransactionViewModel
-        {
-          FromInventory = inventoriesInDb,
-          ToInventory = inventoriesInDb,
-          Products = productsInDb
-        };
-
-        return View("TransactionsForm", viewModel);
-      }
-
-      var fromStockInDb = db.Stocks.Include(i => i.Inventory)
-              .Include(p => p.Product)
-              .Where(i => i.InventoryId == transaction.FromInventoryId)
-              .Single(p => p.ProductId == transaction.ProductId);
-
-      if(fromStockInDb == null)
-      {
-        var inventoriesInDb = db.Inventories.ToList();
-        var productsInDb = db.Products.ToList();
-
-        var viewModel = new TransactionViewModel
-        {
-          FromInventory = inventoriesInDb,
-          ToInventory = inventoriesInDb,
-          Products = productsInDb
-        };
-
-        return View("TransactionsForm", viewModel);
-      }
-
-      else
-        fromStockInDb.Quantity -= transaction.Quantity;
-
-      var toStockInDb = db.Stocks.Include(s => s.Inventory)
-          .Include(s => s.Product)
-          .Where(s => s.InventoryId == transaction.ToInventoryId)
-          .SingleOrDefault(s => s.ProductId == transaction.ProductId);
-
-      if(toStockInDb == null)
-      {
-        var toStock = new Stock
-        {
-          InventoryId = transaction.ToInventoryId,
-          ProductId = transaction.ProductId,
-          Quantity = transaction.Quantity
-        };
-
-        db.Stocks.Add(toStock);
-      }
-
-      else
-        toStockInDb.Quantity += transaction.Quantity;
-
-      transaction.Updated = DateTime.Now;
-      transaction.ByUser = User.Identity.Name;
-      db.Transactions.Add(transaction);
-
-      db.SaveChanges();
-
-      return RedirectToAction("Index", "Transactions");
-    }
-
-    protected override void Dispose(bool disposing)
-    {
-      if(disposing)
-      {
-        db.Dispose();
-      }
-      base.Dispose(disposing);
-    }
-  }
 }
