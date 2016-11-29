c91d034870d2433bc2be3aa27a842493f20addef
VictorBusk <victorbusk@hotmail.com>
2016-11-29 16:04:22 +0100

    Updated JS' again

--- I4PRJ SmartStorage/Scripts/site/datatables/finishStatus.js
+++ I4PRJ SmartStorage/Scripts/site/datatables/finishStatus.js
@@ -1,4 +1,11 @@
-﻿$(document)
+﻿function calDiff(id) {
+    var expected = document.getElementById('expected' + id).textContent;
+    var curQuantity = document.getElementById('curQuantity' + id).value;
+    var result = curQuantity - expected;
+    document.getElementById('diff' + id).innerHTML = result;
+}
+
+$(document)
     .ready(function () {
         var vm = {
             quantities: [],
@@ -12,6 +19,7 @@
                     url: "/api/status/GetStatus/" + document.location.pathname.split('/')[3],
                     dataSrc: ""
                 },
+                rowId: "product.productId",
                 dom: '<"html5buttons"B>lTfgitp',
                 "buttons": [
                     {
@@ -24,7 +32,7 @@
                             { extend: 'pdf', title: 'Optællingsrapport' },
                             {
                                 extend: 'print',
-                                customize: function(win) {
+                                customize: function (win) {
                                     $(win.document.body).addClass('white-bg');
                                     $(win.document.body).css('font-size', '10px');
 
@@ -52,22 +60,28 @@
                         data: "product.category.name"
                     },
                     {
-                        data: "quantity"
+                        data: "quantity",
+                        render: function (data, type, row, meta) {
+                            return "<div id='expected" + meta.row + "' value='" + data + "'>" + data + "</div>";
+                        }
                     },
                     {
-                        data: null,
-                        render: function() {
-                            return "<input type='number' min='0' step='0.25' style='width: 100%' id='curQuantity' class='form-control quantity'/>";
+                        data: "product.productId",
+                        render: function (data, type, row, meta) {
+                            return "<input type='number' min='0' step='0.25' style='width: 100%' id='curQuantity" + meta.row + "' class='form-control quantity' onChange='calDiff(" + meta.row + ");'/>";
                         }
                     },
                     {
                         data: null,
-                        "sDefaultContent": ""
+                        "sDefaultContent": "",
+                        render: function (data, type, row, meta) {
+                            return "<label id='diff" + meta.row + "'></label>";
+                        }
                     },
                     {
                         data: "product.productId",
                         "visible": false,
-                        render: function(data) {
+                        render: function (data) {
                             return "<label id='productIdLabel' value='data'>" + data + "</label>";
                         }
                     }
@@ -105,19 +119,5 @@
                 .fail(function () {
                     toastr.error("Something unexpected happened!");
                 });
-        });
-
-
-        $('.js-refresh').on('click', function () {
-
-            var rows = $('#status').dataTable().fnGetNodes();
-
-            for (var i = 0; i < rows.length; i++) {
-                var expected = table.cell(i, 2).data();
-                var current = table.cell(i, 3).data();
-                var diff = new Number(expected) - new Number(current);
-                table.cell(i, 4).text(diff);
-                toastr.info("Differences has been updated.");
-            }            
         });
     });
