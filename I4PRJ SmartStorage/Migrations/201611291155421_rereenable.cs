7f0adcc31de36ec1b932ff9cfd34b7f261b999a5
Richard Joakim Key <201500155@post.au.dk>
2016-11-29 13:46:06 +0100

    minorupdates

--- I4PRJ SmartStorage/Migrations/201611291155421_rereenable.cs
+++ I4PRJ SmartStorage/Migrations/201611291155421_rereenable.cs
@@ -3,7 +3,7 @@ namespace I4PRJ_SmartStorage.Migrations
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class NullableFromInventoryId : DbMigration
+    public partial class rereenable : DbMigration
     {
         public override void Up()
         {
