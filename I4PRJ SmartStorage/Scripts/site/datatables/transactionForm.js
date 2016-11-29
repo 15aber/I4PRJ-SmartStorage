c4f7eee9424205320b9e639afa1dd0e2f6ed3c00
Andreas Lüdemann <andreaslydemann@gmail.com>
2016-11-29 12:38:03 +0100

    Transaction bug fixes

--- I4PRJ SmartStorage/Scripts/site/datatables/transactionForm.js
+++ I4PRJ SmartStorage/Scripts/site/datatables/transactionForm.js
@@ -1,46 +1,51 @@
 ﻿$(document)
     .ready(function () {
 
+        // apply changes if modelstate fails
+        if ($('#FromInventoryId').val() !== null) {
+            dummy();
+        };
+
         // add eventlistener for 
         $('#FromInventoryId')
-            .on('change', function () {
+            .on('change', dummy);
 
-                // ajax get products of inventory
-                $.get('/api/Inventories/GetOtherInventories/' + this.value,
-                    function (data) {
-                        // populate with options
-                        var inventoriesWithoutFromInventorySelect = $('#ToInventoryId');
-                        // remove selected from select
+        function dummy() {
 
-                        inventoriesWithoutFromInventorySelect.empty();
+            // ajax get products of inventory
+            $.get('/api/Inventories/GetOtherInventories/' + this.value,
+                function (data) {
+                    // populate with options
+                    var inventoriesWithoutFromInventorySelect = $('#ToInventoryId');
 
-                        if (!data || data.length === 0) {
-                            inventoriesWithoutFromInventorySelect.append(new Option('Please choose', undefined));
-                            return;
-                        }
+                    // remove selected from select
+                    inventoriesWithoutFromInventorySelect.empty();
 
-                        for (var i = 0; i < data.length; i++) {
-                            inventoriesWithoutFromInventorySelect.append(new Option(data[i].Name, data[i].InventoryId));
-                        }
-                    });
+                    /*if (!data || data.length === 0) {
+                        productsOfInventorySelect.append(new Option('Please choose', undefined));
+                        return; }*/
 
-                // ajax get products of inventory
-                $.get('/api/Products/GetProductsOfInventory/' + this.value,
-                    function (data) {
-                        // populate with options
-                        var productsOfInventorySelect = $('#ProductId');
-                        // remove selected from select
+                    for (var i = 0; i < data.length; i++) {
+                        inventoriesWithoutFromInventorySelect.append(new Option(data[i].Name, data[i].InventoryId));
+                    }
+                });
 
-                        productsOfInventorySelect.empty();
+            // ajax get products of inventory
+            $.get('/api/Products/GetProductsOfInventory/' + this.value,
+                function (data) {
+                    // populate with options
+                    var productsOfInventorySelect = $('#ProductId');
 
-                        if (!data || data.length === 0) {
-                            productsOfInventorySelect.append(new Option('Please choose', undefined));
-                            return;
-                        }
+                    // remove selected from select
+                    productsOfInventorySelect.empty();
 
-                        for (var i = 0; i < data.length; i++) {
-                            productsOfInventorySelect.append(new Option(data[i].Name, data[i].ProductId));
-                        }
-                    });
-            });
+                    /*if (!data || data.length === 0) {
+                        productsOfInventorySelect.append(new Option('Please choose', undefined));
+                        return; }*/
+
+                    for (var i = 0; i < data.length; i++) {
+                        productsOfInventorySelect.append(new Option(data[i].Name, data[i].ProductId));
+                    }
+                });
+        };
     });
