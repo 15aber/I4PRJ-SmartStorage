4d4d2305ef363375071f408647b3b299dbdb4fbf
Jeppe Stærk <jeppe@staerk.io>
2016-12-03 21:10:34 +0100

    Products layout fix

--- I4PRJ SmartStorage/Scripts/site/datatables/ReadOnlyproductsIndex.js
+++ I4PRJ SmartStorage/Scripts/site/datatables/ReadOnlyproductsIndex.js
@@ -3,35 +3,6 @@
 
         var table = $("#products")
             .DataTable({
-                paging: false,
-                searching: true,
-                "info": false,
-                "ordering": true,
-                dom: '<"html5buttons"B>lTfgitp',
-                "buttons": [
-                    {
-                        extend: 'collection',
-                        text: 'Export',
-                        buttons: [
-                            { extend: 'copy' },
-                            { extend: 'csv' },
-                            { extend: 'excel', title: 'SmartStorage - Produkt' },
-                            { extend: 'pdf', title: 'SmartStorage - Produkt' },
-                            {
-                                extend: 'print',
-                                customize: function (win) {
-                                    $(win.document.body).addClass('white-bg');
-                                    $(win.document.body).css('font-size', '10px');
-
-                                    $(win.document.body)
-                                        .find('table')
-                                        .addClass('compact')
-                                        .css('font-size', 'inherit');
-                                }
-                            }
-                        ]
-                    }
-                ],
                 ajax: {
                     url: "/api/products/getproductsofcategory/" + document.location.pathname.split('/')[3],
                     dataSrc: ""
--- I4PRJ SmartStorage/Scripts/site/datatables/productsIndex.js
+++ I4PRJ SmartStorage/Scripts/site/datatables/productsIndex.js
@@ -3,35 +3,6 @@
 
         var table = $("#products")
             .DataTable({
-                paging: false,
-                searching: true,
-                "info": false,
-                "ordering": true,
-                dom: '<"html5buttons"B>lTfgitp',
-                "buttons": [
-                    {
-                        extend: 'collection',
-                        text: 'Export',
-                        buttons: [
-                            { extend: 'copy' },
-                            { extend: 'csv' },
-                            { extend: 'excel', title: 'SmartStorage - Produkt' },
-                            { extend: 'pdf', title: 'SmartStorage - Produkt' },
-                            {
-                                extend: 'print',
-                                customize: function (win) {
-                                    $(win.document.body).addClass('white-bg');
-                                    $(win.document.body).css('font-size', '10px');
-
-                                    $(win.document.body)
-                                        .find('table')
-                                        .addClass('compact')
-                                        .css('font-size', 'inherit');
-                                }
-                            }
-                        ]
-                    }
-                ],
                 ajax: {
                     url: "/api/products/getproductsofcategory/" + document.location.pathname.split('/')[3],
                     dataSrc: ""
@@ -60,8 +31,8 @@
                     {
                         data: "productId",
                         render: function (data) {
-                            return "<button class='btn btn-primary btn-xs js-edit' data-product-id=" + data + ">Edit</button>" +
-                                "<button class='btn btn-white btn-xs js-delete' data-product-id=" + data + ">Delete</button>";
+                            return "<button class='btn btn-info js-edit' data-product-id=" + data + ">Edit</button> " +
+                                "<button class='btn btn-danger js-delete' data-product-id=" + data + ">Delete</button>";
                         }
                     }
                 ]
