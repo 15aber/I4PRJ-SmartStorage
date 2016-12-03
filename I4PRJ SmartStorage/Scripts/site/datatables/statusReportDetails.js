10e941404ef849dd953ccd60d111d919ae3f9cad
Jeppe Stærk <jeppe@staerk.io>
2016-12-03 21:27:21 +0100

    settings moved to global datatables settings

--- I4PRJ SmartStorage/Scripts/site/datatables/statusReportDetails.js
+++ I4PRJ SmartStorage/Scripts/site/datatables/statusReportDetails.js
@@ -6,31 +6,6 @@
                     url: "/api/status/GetStatus/" + document.location.pathname.split('/')[3],
                     dataSrc: ""
                 },
-                dom: '<"html5buttons"B>lTfgitp',
-                "buttons": [
-                    {
-                        extend: 'collection',
-                        text: 'Export',
-                        buttons: [
-                            { extend: 'copy' },
-                            { extend: 'csv' },
-                            { extend: 'excel', title: 'Optællingsrapport' },
-                            { extend: 'pdf', title: 'Optællingsrapport' },
-                            {
-                                extend: 'print',
-                                customize: function (win) {
-                                    $(win.document.body).addClass('white-bg');
-                                    $(win.document.body).css('font-size', '10px');
-
-                                    $(win.document.body)
-                                        .find('table')
-                                        .addClass('compact')
-                                        .css('font-size', 'inherit');
-                                }
-                            }
-                        ]
-                    }
-                ],
                 "columnDefs": [
                     { "width": "20%", "targets": 0 },
                     { "width": "20%", "targets": 1 },
