d8b80931aa4945e91ab86e8bf17f79b7ff3e8446
Richard Joakim Key <201500155@post.au.dk>
2016-12-10 15:09:45 +0100

    RepositoryTest

--- I4PRJ SmartStorage.Tests/Repository/RepositoryTest.cs
+++ I4PRJ SmartStorage.Tests/Repository/RepositoryTest.cs
@@ -3,17 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NSubstitute;
 using NUnit.Framework;
+using SmartStorage.DAL.Interfaces.Repositories;
+using SmartStorage.DAL.Models;
+using SmartStorage.DAL.Repositories;
 
 namespace I4PRJ_SmartStorage.UnitTests.Repository
 {
     [TestFixture]
     class RepositoryTest
     {
+        private IRepository<Category> _repository;
+
         [SetUp]
         public void SetUp()
         {
-            
+            _repository = Substitute.For<IRepository<Category>>();
+            _repository.GetAll().Returns(new List<Category>
+            {
+                new Category()
+                {
+                    Name = "Beer",
+                    ByUser = "Admin",
+                    CategoryId = 1,
+                    IsDeleted = false,
+                    Updated = DateTime.Today
+                },
+
+                new Category()
+                {
+                    Name = "Soft drinks",
+                    ByUser = "Admin",
+                    CategoryId = 2,
+                    IsDeleted = false,
+                    Updated = DateTime.Today
+                }
+            });
+        }
+
+        [Test]
+        public void GetAll_Returns_TwoCategories()
+        {
+            Assert.That(_repository.GetAll().Count, Is.EqualTo(2));
         }
     }
 }
