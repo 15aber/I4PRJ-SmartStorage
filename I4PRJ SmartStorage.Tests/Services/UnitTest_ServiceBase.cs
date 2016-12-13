3a3bfe72c4a11bd4e4790091def925c8a69c80cc
VictorBusk <victorbusk@hotmail.com>
2016-12-13 10:40:31 +0100

    Setup new test. Not done and failing

--- I4PRJ SmartStorage.Tests/Services/UnitTest_ServiceBase.cs
+++ I4PRJ SmartStorage.Tests/Services/UnitTest_ServiceBase.cs
@@ -1,55 +1,33 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using AutoMapper;
-//using NSubstitute;
-//using NUnit.Framework;
-//using SmartStorage.BLL.Dtos;
-//using SmartStorage.BLL.Mapping;
-//using SmartStorage.BLL.Services;
-//using SmartStorage.DAL.Interfaces;
-//using SmartStorage.DAL.Models;
+﻿using AutoMapper;
+using NSubstitute;
+using NUnit.Framework;
+using SmartStorage.BLL.Dtos;
+using SmartStorage.BLL.Mapping;
+using SmartStorage.BLL.Services;
+using SmartStorage.DAL.Interfaces;
+using SmartStorage.DAL.Models;
 
-//namespace SmartStorage.UnitTests.Services
-//{
-//    [TestFixture]
-//    class UnitTest_ServiceBase
-//    {
-//        private IUnitOfWork _uow;
-//        private CategoryService _categoryService;
+namespace SmartStorage.UnitTests.Services
+{
+    [TestFixture]
+    class UnitTest_ServiceBase
+    {
+        private IUnitOfWork _uow;
+        private ServiceBase<TransactionDto> _serviceBase;
 
-//        [SetUp]
-//        public void SetUp()
-//        {
-//            _uow = Substitute.For<IUnitOfWork>();
-//            Mapper.Initialize(c => c.AddProfile<MappingProfile>());
-//            _categoryService = new CategoryService(_uow);
-//        }
+        [SetUp]
+        public void SetUp()
+        {
+            const string BAD_INPUT = "bad input";
+            _uow = Substitute.For<IUnitOfWork>();
+            Mapper.Initialize(c => c.AddProfile<MappingProfile>());
+        }
 
-//        [Test]
-//        public void CategoryServiceAdd_UnitOfWorkAddAndComplete_ReturnsUnitOfWorkAddAndComplete()
-//        {
-//            var categoryDto = new CategoryDto() { Name = "Test" };
-//            var entity = Mapper.Map<CategoryDto, Category>(categoryDto);
-
-//            _categoryService.Add(categoryDto);
-
-//            _uow.Received().Categories.Add(entity);
-//            _uow.Received().Complete();
-//        }
-
-//        [Test]
-//        public void CategoryServiceUpdate_UnitOfWorkUpdateAndComplete_ReturnsUnitOfWorkUpdateAndComplete()
-//        {
-//            var categoryDto = new CategoryDto() { Name = "Test" };
-//            var entity = Mapper.Map<CategoryDto, Category>(categoryDto);
-
-//            _categoryService.Update(categoryDto);
-
-//            _uow.Received().Categories.Update(entity);
-//            _uow.Received().Complete();
-//        }
-//    }
-//}
+        //[Test]
+        //public void ThrowsExceptionCorrectly()
+        //{
+        //    _serviceBase.Add(new TransactionDto());
+        //    Assert.Fail("No exception was thrown");       
+        //}
+    }
+}
--- I4PRJ SmartStorage.Tests/Services/UnitTest_TransactionService.cs
+++ I4PRJ SmartStorage.Tests/Services/UnitTest_TransactionService.cs
@@ -1,55 +1,51 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using AutoMapper;
-//using NSubstitute;
-//using NUnit.Framework;
-//using SmartStorage.BLL.Dtos;
-//using SmartStorage.BLL.Mapping;
-//using SmartStorage.BLL.Services;
-//using SmartStorage.DAL.Interfaces;
-//using SmartStorage.DAL.Models;
+﻿using AutoMapper;
+using NSubstitute;
+using NUnit.Framework;
+using SmartStorage.BLL.Dtos;
+using SmartStorage.BLL.Mapping;
+using SmartStorage.BLL.Services;
+using SmartStorage.DAL.Interfaces;
+using SmartStorage.DAL.Models;
 
-//namespace SmartStorage.UnitTests.Services
-//{
-//    [TestFixture]
-//    class UnitTest_TransactionService
-//    {
-//        private IUnitOfWork _uow;
-//        private CategoryService _categoryService;
+namespace SmartStorage.UnitTests.Services
+{
+    [TestFixture]
+    class UnitTest_TransactionService
+    {
+        private IUnitOfWork _uow;
+        private TransactionService _transactionservice;
 
-//        [SetUp]
-//        public void SetUp()
-//        {
-//            _uow = Substitute.For<IUnitOfWork>();
-//            Mapper.Initialize(c => c.AddProfile<MappingProfile>());
-//            _categoryService = new CategoryService(_uow);
-//        }
+        [SetUp]
+        public void SetUp()
+        {
+            _uow = Substitute.For<IUnitOfWork>();
+            Mapper.Initialize(c => c.AddProfile<MappingProfile>());
+            _transactionservice = new TransactionService(_uow);
+        }
 
-//        [Test]
-//        public void CategoryServiceAdd_UnitOfWorkAddAndComplete_ReturnsUnitOfWorkAddAndComplete()
-//        {
-//            var categoryDto = new CategoryDto() { Name = "Test" };
-//            var entity = Mapper.Map<CategoryDto, Category>(categoryDto);
+        //[Test]
+        //public void transactionserviceAdd_UnitOfWorkAddAndComplete_ReturnsUnitOfWorkAddAndComplete()
+        //{
+        //    var transactionDto = new TransactionDto() {FromInventoryId = 1, Quantity = 1};
+        //    var entity = Mapper.Map<TransactionDto, Transaction> (transactionDto);
+        //    _uow.Stocks.GetSingle().Quantity.Returns(1);
 
-//            _categoryService.Add(categoryDto);
+        //    _transactionservice.Add(transactionDto);
 
-//            _uow.Received().Categories.Add(entity);
-//            _uow.Received().Complete();
-//        }
+        //    _uow.Received().Transactions.Add(entity);
+        //    _uow.Received().Complete();
+        //}
 
-//        [Test]
-//        public void CategoryServiceUpdate_UnitOfWorkUpdateAndComplete_ReturnsUnitOfWorkUpdateAndComplete()
-//        {
-//            var categoryDto = new CategoryDto() { Name = "Test" };
-//            var entity = Mapper.Map<CategoryDto, Category>(categoryDto);
+        //[Test]
+        //public void transactionserviceUpdate_UnitOfWorkUpdateAndComplete_ReturnsUnitOfWorkUpdateAndComplete()
+        //{
+        //    var transactionDto = new TransactionDto();
+        //    var entity = Mapper.Map<TransactionDto, Transaction>(transactionDto);
 
-//            _categoryService.Update(categoryDto);
+        //    _transactionservice.Update(transactionDto);
 
-//            _uow.Received().Categories.Update(entity);
-//            _uow.Received().Complete();
-//        }
-//    }
-//}
+        //    _uow.Received().Transactions.Update(entity);
+        //    _uow.Received().Complete();
+        //}
+    }
+}
