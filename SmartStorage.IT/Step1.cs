d2cdba0139f4f0dc7d74c6a3f8f807204577aa01
VictorBusk <victorbusk@hotmail.com>
2016-12-15 14:24:44 +0100

    Step 1 updated and not done

--- SmartStorage.IT/Step1.cs
+++ SmartStorage.IT/Step1.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Runtime.Remoting.Contexts;
 using System.Web.Mvc;
 using AutoMapper;
 using NSubstitute;
@@ -12,11 +11,14 @@ using SmartStorage.BLL.Dtos;
 using SmartStorage.BLL.Mapping;
 using SmartStorage.BLL.Services;
 using SmartStorage.DAL.Context;
-using SmartStorage.DAL.Interfaces;
 using SmartStorage.DAL.Models;
 using SmartStorage.DAL.UnitOfWork;
 using SmartStorage.UI.Controllers;
 using SmartStorage.UI.ViewModels;
+using System.Web;
+using System.Web.Routing;
+using SmartStorage.DAL.Interfaces.Repositories;
+using SmartStorage.DAL.Repositories;
 
 namespace SmartStorage.IT
 {
@@ -28,15 +30,32 @@ namespace SmartStorage.IT
     private List<Category> categoryList;
     private CategoryDto singleCategoryDto;
     private CategoriesController _categoriesController;
-    private ApplicationDbContext _dbContext;
+    private HttpContextBase _contextBase;
+    private ApplicationDbContext _context;
+    private Repository<Category> _repository;
+    private DbSet<Category> _dbSet;
+    protected DbContext _dbContext;
+
 
     [SetUp]
     public void SetUp()
     {
-      _dbContext = Substitute.For<ApplicationDbContext>();
-      _uow = new UnitOfWork(_dbContext);
+      _dbSet = Substitute.For<DbSet<Category>>();
+      _context = Substitute.For<ApplicationDbContext>();
+      _dbContext = _context;
+      _uow = new UnitOfWork(_context);
+      _repository = new Repository<Category>(_dbContext);
       Mapper.Initialize(c => c.AddProfile<MappingProfile>());
       _categoryService = new CategoryService(_uow);
+      _categoriesController = new CategoriesController(_categoryService);
+
+      _contextBase = Substitute.For<HttpContextBase>();
+      Mapper.Initialize(c => c.AddProfile<MappingProfile>());
+
+      _contextBase.User.Identity.Name.Returns("JohnDoe");
+      _contextBase.Request.IsAuthenticated.Returns(true);
+      _contextBase.User.IsInRole("Admin").Returns(true);
+      _categoriesController.ControllerContext = new ControllerContext(_contextBase, new RouteData(), _categoriesController);
 
       categoryList = new List<Category>
             {
@@ -72,7 +91,15 @@ namespace SmartStorage.IT
         Category = categoryDto
       };
 
+      _repository = new CategoriesRepository(_context);
+
+      var entity = Mapper.Map<CategoryDto, Category>(categoryDto);
+
+      _dbSet.Add(entity).Returns(entity);
+
       _categoriesController.Create(viewModel);
+
+      _uow.Categories.Add(entity);
 
       _categoryService.Received().Add(viewModel.Category);
 
